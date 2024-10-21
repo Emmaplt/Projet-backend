fc74891d575acd904875ae54bcef10fe02100c1d
Emma Plaut <emma.plaut@hotmail.fr>
2024-10-21 12:16:30 +0200

    initialisation du projet

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  console.log('Request Headers:', req.headers); 
+
+  try {
+    if (!req.headers.authorization) {
+      console.log('Authorization header is missing');
+      return res.status(401).json({ message: 'Authorization header missing' });
+    }
+
+    const token = req.headers.authorization.split(' ')[1];
+    console.log('Token reçu:', token);  
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token missing' });
+    }
+
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');  
+    const userId = decodedToken.userId;
+    req.auth = { userId: userId };
+    next();
+  } catch (error) {
+    console.error('JWT Error:', error.message);
+    return res.status(401).json({ message: 'Authentication failed', error: error.message });
+  }
+};
---
+++ routes/book.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+const multer = require('../middleware/multer-config');
+
+const bookCtrl = require('../controllers/book');
+
+router.get('/', bookCtrl.getAllBook);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.get('/bestrating', bookCtrl.getBestRatedBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+
+module.exports = router;
